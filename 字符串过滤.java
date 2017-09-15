25885f27849c54c4fd8f73fb5325690d48dadbe5
lmjssjj <524779129@qq.com>
2017-09-15 10:41:49 +0800

    Create 字符串过滤.java

---
+++ 字符串过滤.java
@@ -0,0 +1,14 @@
+private String getFilteredQueryString(CharSequence query) {
+        if (query == null) {
+            return null;
+        }
+        final StringBuilder filtered = new StringBuilder();
+        for (int n = 0; n < query.length(); n++) {
+            char c = query.charAt(n);
+            if (!Character.isLetterOrDigit(c) && !Character.isSpaceChar(c)) {
+                continue;
+            }
+            filtered.append(c);
+        }
+        return filtered.toString();
+    }
