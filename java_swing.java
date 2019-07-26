5005a1a559b161547ef1e09c4bff975b213f8797
lmjssjj <524779129@qq.com>
2019-07-26 10:43:29 +0800

    Update java_swing.java

--- java_swing.java
+++ java_swing.java
@@ -1,5 +1,7 @@
 try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");// 使用windows外观
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");// 使用windows外观
+} catch (Exception e) {
+	e.printStackTrace();
+}
+this.setIconImage(Toolkit.getDefaultToolkit().getImage("src/images/nuu_icon.png"));//设置窗口icon
+
