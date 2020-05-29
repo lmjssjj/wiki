b5061c7ac2a66b6c88aa43039ce4c62b2f971abd
lmjssjj <524779129@qq.com>
2020-05-29 12:33:30 +0800

    utils code

---
+++ android system/mbldenv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ##########################################################
+# ALPS(Android4.1 based) build environment profile setting
+# ##########################################################
+# Overwrite JAVA_HOME environment variable setting if already exists
+# JAVA_HOME=/mtkoss/jdk/1.6.0_45-ubuntu-10.04/x86_64
+JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export JAVA_HOME
+
+# Overwrite ANDROID_JAVA_HOME environment variable setting if already exists
+# ANDROID_JAVA_HOME=/mtkoss/jdk/1.6.0_45-ubuntu-10.04/x86_64
+ANDROID_JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export ANDROID_JAVA_HOME
+
+# Overwrite PATH environment setting for JDK & arm-eabi if already exists
+#PATH=/mtkoss/jdk/1.6.0_45-ubuntu-10.04/x86_64/bin:$PWD/prebuilts/gcc/linux-x86/arm/cit-arm-linux-androideabi-4.8/bin:$PWD/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin:$PWD/prebuilts/gcc/linux-x86/aarch64/linaro-aarch64-linux-gnu-4.8/bin:$PWD/prebuilts/misc/linux-x86/make:$PATH
+PATH=/usr/lib/jvm/java-7-openjdk-amd64/bin:$PWD/prebuilts/gcc/linux-x86/arm/cit-arm-linux-androideabi-4.8/bin:$PWD/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin:$PWD/prebuilts/gcc/linux-x86/aarch64/linaro-aarch64-linux-gnu-4.8/bin:$PWD/prebuilts/misc/linux-x86/make:$PATH
+export PATH
+
+# Add MediaTek developed Python libraries path into PYTHONPATH
+if [ -z "$PYTHONPATH" ]; then
+  PYTHONPATH=$PWD/mediatek/build/tools
+else
+  PYTHONPATH=$PWD/mediatek/build/tools:$PYTHONPATH
+fi
+export PYTHONPATH
+
