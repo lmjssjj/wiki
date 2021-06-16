f72a39a36929b1fef045b72e371c79ee8f5deeaa
lmjssjj <524779129@qq.com>
2021-06-16 09:30:17 +0800

    add setup wizard

---
+++ android system/开机向导/SetupWizardExt/app/src/main/java/com/lmjssjj/setupwizardext/PartnerReceiver.java
@@ -0,0 +1,17 @@
+package com.lmjssjj.setupwizardext;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by lmjssjj on 2018/4/25.
+ * setup receiver
+ * frameworks/opt/setupwizard/
+ */
+public class PartnerReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
